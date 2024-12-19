c31756b38054b3feeefdc210c8bc38b2fee6d03f
eatsleepeatsleep <107299571+eatsleepeatsleep@users.noreply.github.com>
2024-12-20 04:19:10 +0800

    Update index.js
    roundedProbability改成truncatedProbability

--- js/index.js
+++ js/index.js
@@ -102,7 +102,7 @@ window.initMap = function () {
                     hospitalElement.classList.add('hospital-card');
                     hospitalElement.innerHTML = `
                         <h3>${hospital.priority}：${hospital.name}</h3>
-                        <p><strong>患者接受正確治療的機率：</strong> ${roundedProbability}</p>
+                        <p><strong>患者接受正確治療的機率：</strong> ${truncatedProbability}</p>
                         <p><strong>從病發到接受正確治療的平均時間：</strong> ${Math.round(meanMinutes)} 分鐘</p>
                         <p><strong>預期最快可接受正確治療的時間：</strong> ${Math.round(lowerboundMinutes)} 分鐘</p>
                         <button onclick="window.open('${hospital.google_map_url}', '_blank')">前往地圖</button>
